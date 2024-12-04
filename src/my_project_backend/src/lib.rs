212e1732443d1e88de189afeae5a44f48b84c724
Mateusz Maślanka <mateuszmaslanka06@gmail.com>
2024-12-04 09:41:31 +0000

    Added save data option

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,3 +1,9 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static MSG: RefCell<String> = RefCell::new(String::new());
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
